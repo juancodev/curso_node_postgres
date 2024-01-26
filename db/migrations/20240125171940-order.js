1fbe88cdf29a38e555ad5b35cf5304c894e6d8f5
juancodev <montillasanchezjuancarlos@gmail.com>
2024-01-26 00:55:01 -0400

    [⚡ADD] changing migrations in order

--- db/migrations/20240125171940-order.js
+++ db/migrations/20240125171940-order.js
@@ -1,7 +1,14 @@
 'use strict';
 
 const {
-  orderSchema,
+  DataTypes,
+  Sequelize
+} = require('sequelize')
+
+const {
+  CUSTOMER_TABLE
+} = require('./../models/customer.model.js')
+const {
   ORDER_TABLE
 } = require('./../models/order.model');
 
@@ -9,7 +16,31 @@ const {
 module.exports = {
   async up(queryInterface) {
 
-    await queryInterface.createTable(ORDER_TABLE, orderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        field: 'created_at',
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    });
   },
 
   async down(queryInterface) {
